47ea371208f8c525aafbf283ecaedbf361d4f389
duduagarcia <eduardo.agarcia117@gmail.com>
2024-03-15 19:28:30 -0300

    feat: added supabase

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: false },
-  modules: ["@nuxt/ui", "@pinia/nuxt", "@nuxtjs/google-fonts"],
+  modules: [
+    "@nuxt/ui",
+    "@pinia/nuxt",
+    "@nuxtjs/google-fonts",
+    "@nuxtjs/supabase",
+  ],
   googleFonts: {
     families: {
       Inter: [400, 500, 600, 700],
     },
+  },
+  supabase: {
+    url: process.env.SUPABASE_URL,
+    key: process.env.SUPABASE_KEY,
+    redirect: false,
   },
 });
